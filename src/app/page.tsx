13e0e919da14eb29c5a52770496258f6494b606a
aryaersiputra8 <aryaersiputra8@gmail.com>
2025-01-28 19:19:26 +0000

    Perbaikan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,11 +40,14 @@ const Page: React.FC = () => {
       const result = await axios.post(url, requestBody, { headers });
 
       const rawResponse = result.data.candidates[0]?.content.parts[0]?.text || "Tidak ada jawaban tersedia.";
+      console.log("Raw Response:", rawResponse)
       const formattedResponse = rawResponse
         .replace(/```([^`]+)```/g, '<pre class="bg-gray-800 p-2 rounded overflow-auto text-white"><code>$1</code></pre>')
         .replace(/\n/g, '<br />')
-        .replace(/\*\*(.*?)\*\*/g, '<span class="text-xl font-bold">$1</span>')
-        .replace(/\*\*\*(.*?)\*\*\*/g, '<span class="text-2xl font-bold">$1</span>')
+        .replace(/\*\*(.*?)\*\*/g, '<span class="text-lg font-bold">$1</span>')
+        .replace(/\*\*\*(.*?)\*\*\*/g, '<span class="text-xl font-bold">$1</span>')
+        .replace(/`(.*?)`/g, '<span class="italic bg-gray-200 px-1">$1</span>')
+        .replace(/^\* (.*?)$/gm, '<ul><li class="list-disc">$1</li></ul>')
 
       setResponse(formattedResponse);
     } catch (error) {
@@ -56,7 +59,7 @@ const Page: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col lg:flex-row items-center justify-center w-full h-screen bg-gradient-to-b from-blue-100 to-blue-300 p-4 gap-6">
+    <div className="flex flex-col lg:flex-row items-center justify-center w-full h-full bg-gradient-to-b from-blue-100 to-blue-300 p-4 gap-6">
       <div className="bg-white p-8 rounded-2xl shadow-xl w-full flex-1 flex flex-col h-full gap-2">
         <div className="w-full h-60 overflow-hidden rounded-lg">
           <img src="https://i.pinimg.com/736x/fa/a6/48/faa648166c1d2978be69b6537fa7024e.jpg" alt="Image" className="object-cover w-full h-full" />
@@ -87,9 +90,9 @@ const Page: React.FC = () => {
 
       <div className="flex-1 w-full bg-white h-full p-6 rounded-2xl shadow-xl items-center mt-6 lg:mt-0">
         {loading ? (
-          <div className="text-center text-blue-500 font-semibold">Memproses pertanyaan Anda...</div>
+          <div className="w-full text-center text-blue-500 font-semibold">Memproses pertanyaan Anda...</div>
         ) : response ? (
-          <div className="mt-6 bg-gray-100 p-4 rounded-lg shadow overflow-y-auto max-h-full">
+          <div className="mt-6 bg-gray-100 p-4 rounded-lg shadow overflow-y-auto max-h-[500px] w-full">
             <h2 className="font-semibold text-lg mb-2">Jawaban:</h2>
             <div
               className="text-gray-700 prose whitespace-pre-wrap break-words"
@@ -100,6 +103,7 @@ const Page: React.FC = () => {
           <div className="text-center text-gray-500">Jawaban akan ditampilkan di sini.</div>
         )}
       </div>
+
     </div>
   );
 };
